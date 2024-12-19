09f59d515ec11897ccf4a5ee6a434740368bc09b
sariyaesc <escamillasara49@gmail.com>
2024-12-18 19:20:57 -0600

    add: add class notes

---
+++ practica.js
@@ -0,0 +1,63 @@
+// const age=18
+// const id=true
+// const palanca= false
+
+// if (age>=18 && id==true || palanca==true) {
+//     console.log("Puedes entrar")
+// } else {
+//     console.log("Te la pelaste")    
+// }
+
+let dormamu_se_rindio=false
+let i=0
+// while (dormamu_se_rindio==true) {
+//     console.log("no se rindio en el intento",i)
+//     i++
+//     if (i==3450) {
+//         dormamu_se_rindio=true
+//     }
+// }
+
+// for (let index = 0; index <=150; index+=2) {
+//     console.log("repito",index);
+    
+// }
+
+// const option=2
+// switch (option) {
+//     case 1:
+//     console.log("holi desde 1")
+//         break;
+//     case 2:
+//         console.log("holi desde 2")
+//         break;
+//     case 3:
+//         console.log("holi desde 3")
+//         break;
+//     default:
+//         console.log("no")
+//         break;
+// }
+
+// const variable={
+//     num:15,
+//     nombre:'Alicia'
+// }
+
+// console.log(variable.nombre)
+
+/*function dividir(a,b){
+    try {
+        if (b===0) {
+            throw new Error("No se puede krnal")
+        }
+        let result=a/b
+        console.log(result)
+    } catch (error) {
+        console.log("Tenemos un error:",error.message)
+    } 
+}
+
+dividir(3,0) 
+*/
+
