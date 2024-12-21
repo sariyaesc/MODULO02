1b5de01f842ac7012d2214cb3aeb20b022465363
sariyaesc <escamillasara49@gmail.com>
2024-12-20 23:30:18 -0600

    feat: add Pract2.6.js

--- Pract2.6.js
+++ Pract2.6.js
@@ -3,14 +3,24 @@ function palindromo(num) {
      let numS=num.toString()
      let secuencia=[...numS]
      console.log(secuencia)
+
      let length=secuencia.length
-    for (let index = 0; index < length; index++) {
-        const element = array[index];
-         if (condition) {
+
+    for (let index = 0; index < length-2; index++) {
+        let a=secuencia[index]
+        let b=secuencia[length-index-1]
+        
+        if (a!==b) {
+            console.log("No es palindromo")
+            return 
+        } 
             
-         }
+    } console.log("Es palindromo")
+
+        }else {
+            console.log("Tiene que ser entero positivo")
         }
     }
-}
+
 
 palindromo(25552)
